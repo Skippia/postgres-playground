76b1c2a2e77f91d163961a076fedbba11560e60f
Skippia <pocketbook.love24@gmail.com>
2025-03-31 18:46:20 +0300

    feat: add usecase

---
+++ init-db/002_init.sh
@@ -0,0 +1,7 @@
+# #!/bin/bash
+
+# cat <<EOT >> /var/lib/postgresql/data/postgresql.conf
+# shared_preload_libraries='pg_cron,pg_ivm'
+# default_text_search_config='english'
+# EOT
+
--- init-db/002_pg-cron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat <<EOT >> /var/lib/postgresql/data/postgresql.conf
-shared_preload_libraries='pg_ivm'
-EOT
